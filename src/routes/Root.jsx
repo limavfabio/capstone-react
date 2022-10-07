1543bb095b3eee40409f74957085a1d8b22765e0
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-10-07 18:07:22 -0300

    Implement search

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGetFruitsQuery } from '../features/fruits/fruitsApiSlice';
 import style from '../style/Root.module.css';
 import FruitBlock from '../components/FruitBlock';
@@ -6,6 +6,9 @@ import Header from '../components/Header';
 import Loading from '../components/Loading';
 
 export default function Root() {
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredFruits, setFilteredFruits] = useState([]);
+
   const {
     data: fruits,
     isLoading,
@@ -14,21 +17,39 @@ export default function Root() {
     error,
   } = useGetFruitsQuery();
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== '') {
+      const filteredData = fruits.filter((item) => {
+        return Object.values(item)
+          .join('')
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredFruits(filteredData);
+    } else {
+      setFilteredFruits(fruits);
+    }
+  };
+
   let content;
 
   if (isLoading) {
     content = <Loading />;
-  } else if (isSuccess) {
+  } else if (isSuccess && searchInput.length > 1) {
+    content = filteredFruits.map((fruit) => (
+      <FruitBlock key={fruit.id} fruit={fruit} />
+    ));
+  } else {
     content = fruits.map((fruit) => (
       <FruitBlock key={fruit.id} fruit={fruit} />
     ));
-  } else if (isError) {
-    content = <div>{error.toString()}</div>;
   }
 
   return (
     <>
       {isSuccess ? <Header /> : ''}
+      <input type="text" onChange={(e) => searchItems(e.target.value)} />
       <section className={style.container}>{content}</section>
     </>
   );
